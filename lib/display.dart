16de0fa3ea9abf9381cc520d89cc31bdc215ad1a
niteshsetti <nitesh.setti2001@gmail.com>
2022-07-18 17:50:43 +0530

    second

--- lib/display.dart
+++ lib/display.dart
@@ -4,27 +4,29 @@ import 'package:first/screens/navi.dart';
 import 'package:flutter/material.dart';
 
 class Display {
-  Widget display(String name, String dept,BuildContext context) {
+  Widget display(String name, String dept, BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: ListTile(
-        contentPadding: EdgeInsetsGeometry.lerp(EdgeInsets.all(5), EdgeInsets.all(5), 0.0),
+        contentPadding:
+            EdgeInsetsGeometry.lerp(EdgeInsets.all(5), EdgeInsets.all(5), 0.0),
         visualDensity: VisualDensity.adaptivePlatformDensity,
-          style: ListTileStyle.list,
-          autofocus: true,
-          leading: Icon(Icons.list),
-          trailing: Icon(Icons.delete),
-          title: Text(name),
-          subtitle: Text(dept),
-          tileColor: Colors.white,
-          textColor: Colors.green,
-          iconColor: Colors.black,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-          onTap: ()
-          {
-            Navigator.push(context,MaterialPageRoute(builder: ((context) => Navi(name,dept))));
-          },
-          ),
+        style: ListTileStyle.list,
+        autofocus: true,
+        leading: Icon(Icons.list),
+        trailing: Icon(Icons.delete),
+        title: Text(name),
+        subtitle: Text(dept),
+        tileColor: Colors.white,
+        textColor: Colors.green,
+        iconColor: Colors.black,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        onTap: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: ((context) => Navi(name, dept))));
+        },
+      ),
     );
   }
 
